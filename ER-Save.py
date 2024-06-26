1b1040a0bb6cdba3959c27c7b4a71bdbe4fe9f94
Brian Hughes <69159137+Ghost-In-The-Void@users.noreply.github.com>
2024-06-26 02:19:22 +0000

    corrected some spelling errors

--- ER-Save.py
+++ ER-Save.py
@@ -1,6 +1,6 @@
 # ER-Save
 # Version: 1.1
-# Elden Ring character save file utility to back up character save files. This program will look for character save files and then copy them if found. This currently works for both Vanilla and Seemless Co-Op save files. Tested on Windows 11 PC.
+# Elden Ring character save file utility to back up character save files. This program will look for character save files and then copy them if found. This currently works for both Vanilla and seamless Co-Op save files. Tested on Windows 11 PC.
 # Designed by Ghost-In-The-Void in 2024
 # https://github.com/Ghost-In-The-Void/ER-Save
 #Libraries used
@@ -8,7 +8,7 @@ import os
 import shutil
 #Variables to define what file types to look for and assigning a "category" to the file type
 extensions = ".sl2"
-seemless_save = ".co2"
+seamless_save = ".co2"
 #Variable to define the path to the games save directory
 directory = os.path.join(os.path.expanduser("~"), r'AppData\Roaming\EldenRing')
 #function to locate the save paths
@@ -45,7 +45,7 @@ for filename in os.listdir(saves_location_str):
             shutil.copy(file_path, destination_path)
             #Printing results of move
             print(f"Copied {filename} to {folder_name} folder.")
-        if extension in seemless_save:
+        if extension in seamless_save:
             folder_name = "Elden Ring Save Backups"
             #Variable to assign path for creating folder
             folder_path2 = os.path.join(back_up_folder, folder_name)
@@ -66,6 +66,6 @@ for filename in os.listdir(saves_location_str):
         print(f"Skipped {filename}. It is a directory.")
 #Successful migration of character files        
 print(f"Character back up completed. Your backup save files are located here ==>" + '\x1b[6;30;42m' + fr"{back_up_folder}\{folder_name}" + '\x1b[0m' "<==")
-print('\x1b[0;30;43m' + "Vanilla save files end in .sl2 and Seemless Co-op saves end in .co2" + '\x1b[0m')
+print('\x1b[0;30;43m' + "Vanilla save files end in .sl2 and seamless Co-op saves end in .co2" + '\x1b[0m')
 input('Press Enter to exit....')
 
